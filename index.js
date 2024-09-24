a17da0f36c686445c4d14ccfd17e86a17048a35e
jatin-rwt <rawatjatin0206@gmail.com>
2024-09-24 22:02:17 +0530

    User Interface added with Server Side Rendering

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const path = require("path")
 const { connectToMongoDB }= require("./connect")
 const urlRoute = require("./routes/url")
+const staticRoute = require('./routes/staticRouter')
 const URL = require('./models/url')
 
 const app = express()
@@ -10,15 +12,20 @@ connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(() => {
     console.log("MongoDB connected");
     
-})
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
 
 
 app.use("/url", urlRoute);
 
-app.get('/:shortId', async (req, res) => {
+app.use("/", staticRoute)
+
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
@@ -29,7 +36,8 @@ app.get('/:shortId', async (req, res) => {
             },
         }
     );
-    res.redirect(entry.redirectURL)
+    //for resolving error 
+    res.redirect(entry?.redirectURL)
 });
 
 app.listen(PORT, () => {
